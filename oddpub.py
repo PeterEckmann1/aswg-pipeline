94319f1b42d2a76b47c7464a52466831fce6439c
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2022-08-08 13:14:31 +0200

    update report format

--- oddpub.py
+++ oddpub.py
@@ -1,32 +1,17 @@
 import subprocess
+import csv
 
 
 def oddpub():
     output = {}
-    subprocess.call(['Rscript', 'utils/oddpub/oddpub.R', 'temp/all_text/', 'temp/oddpub.csv'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    subprocess.call(['Rscript', 'utils/oddpub/oddpub.R', 'temp/all_text/', 'temp/oddpub.csv'])#, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     f = open('temp/oddpub.csv', 'r')
     next(f)
-    for line in f:
-        doi = line.split()[1].replace('"', '').replace('.txt', '').replace('_', '/')
-        open_data = None
-        open_code = None
-        for col in line.split():
-            if col in ['FALSE', 'TRUE']:
-                if open_data is None:
-                    open_data = col == 'TRUE'
-                else:
-                    open_code = col == 'TRUE'
-                    break
-        if open_data:
-            if open_code:
-                statement = 'Thank you for sharing your code and data.'
-            else:
-                statement = 'Thank you for sharing your data.'
-        else:
-            if open_code:
-                statement = 'Thank you for sharing your code.'
-            else:
-                statement = 'We did not detect open data. We also did not detect open code. Researchers are encouraged to share open data when possible (see <a href="http://blogs.nature.com/naturejobs/2017/06/19/ask-not-what-you-can-do-for-open-data-ask-what-open-data-can-do-for-you/">Nature blog</a>).'
-        statement = '<p><i>Results from <a href="https://www.biorxiv.org/content/10.1101/2020.05.11.088021v1">OddPub</a></i>: {}</p>'.format(statement)
-        output[doi] = {'html': statement, 'open_code': open_code, 'open_data': open_data}
+    for row in csv.reader(f, delimiter=' '):
+        import sys
+        print(row, file=sys.stderr)
+        doi = row[1].replace('"', '').replace('.txt', '').replace('_', '/')
+        open_data = row[2] == 'TRUE'
+        open_code = row[4] == 'TRUE'
+        output[doi] = {'open_code': open_code, 'open_data': open_data, 'open_data_statement': row[5], 'open_code_statemenent': row[6]}
     return output
